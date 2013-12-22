29abba09690086eabaeb5bbbcdbd95191a1a0a53
D1maka <veres.dima@gmail.com>
2013-12-22 19:26:59 +0200

    ohoho

---
+++ CampusGroups/InviteUserControl.ascx
@@ -0,0 +1,11 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="InviteUserControl.ascx.cs" Inherits="CampusGroups.InviteUserControl" %>
+<asp:Image ID="avatarImg" runat="server" />
+&nbsp;<asp:Label ID="usernameLbl" runat="server" Text=""></asp:Label>
+&nbsp;
+<br />
+<asp:Label ID="userProfileType" runat="server" Text=""></asp:Label>
+<br />
+<asp:Label ID="userDepartment" runat="server" Text=""></asp:Label>
+<br />
+<asp:ImageButton ID="inviteBtn" runat="server" />
+<asp:DropDownList ID="DropDownListRoles" runat="server"></asp:DropDownList>
---
+++ CampusGroups/InviteUserControl.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CampusGroups.dao;
+using CampusGroups.data;
+
+namespace CampusGroups
+{
+    public partial class InviteUserControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["userCampusId"] == null)
+            {
+                Response.Redirect("~/LoginPage.aspx");
+            }
+            else
+            {
+                int id = Int32.Parse(Session["userCampusId"].ToString());
+                UserDAO usrDAO = UserDAO.getUserDAO();
+                User user = usrDAO.getGroupsUserByCampusAccountId(id);
+                String name = usrDAO.getUserNameByCampusUserAccountId(id);
+                String profile = usrDAO.getProfileNameByCampusUserAccountId(id);
+                String department = usrDAO.getSubdivisionNameByCampusUserAccountId(id);
+                this.usernameLbl.Text = name;
+                this.userProfileType.Text = profile;
+                this.userDepartment.Text = department;
+                this.avatarImg.Height = 128;
+                this.avatarImg.Width = 128;
+                this.avatarImg.ImageUrl = user.avatarLink;
+            }
+        }
+    }
+}
